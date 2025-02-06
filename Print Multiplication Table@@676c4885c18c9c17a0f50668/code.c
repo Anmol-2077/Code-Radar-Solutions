d4716d3845bb5308d857a91806a0c2d81a619150
Anmol Gupta <anmol0042.becse24@chitkara.edu.in>
2025-02-06 09:58:47 +0530

    Solution to Print Multiplication Table

---
+++ Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int i;
+    for(i=1,i<=10;i++){
+        printf("%d X %d = %d\n",n,i,n*i);
+    }
+    return 0;
+}
